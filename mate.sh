5dbc8a621697804fc3b078b7de4a2de34e793797
Ariel DerKonig <ariel.derkonig@gmail.com>
2022-03-28 03:27:42 -0300

    initial commit

---
+++ mate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#===================================================================
+# INSTALLING MATE DESKTOP ENVIRONMENT - DEBIAN 10
+#===================================================================
+apt-get install -y xserver-xorg-core sddm open-vm-tools
+apt-get install -y --no-install-recommends mate-desktop-environment-core mate-menu
+apt-get install -y mate-tweak network-manager-gnome network-manager-openvpn-gnome
+apt-get install -y sakura gnupg wget git zip unzip htop
+apt-get install -y eom pluma mate-calc engrampa
+apt-get install -y breeze-cursor-theme fonts-inconsolata fonts-roboto
+apt-get install -y alsa-tools alsa-utils alsa-oss
+
+dpkg -i ./source/paper-icon-theme_1.5.0_all.deb
+dpkg -i ./source/deepin-gtk-theme_17.10.11_all.deb
+
+#============ INSTALL LIBREOFFICE
+apt-get install -y libreoffice-calc --no-install-recommends
+apt-get install -y libreoffice-writer --no-install-recommends
+apt-get install -y libreoffice-gtk2
+
+#============ INSTALL BRAVE-BROWSER
+apt-get install -y apt-transport-https gnupg
+apt-key adv --fetch-keys https://brave-browser-apt-release.s3.brave.com/brave-core.asc
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ buster main" > /etc/apt/sources.list.d/brave-browser.list
+apt-get update && apt-get install -y brave-keyring brave-browser
+
+#============ INSTALL SPOTIFY
+apt-key adv --fetch-keys https://download.spotify.com/debian/pubkey_0D811D58.gpg
+echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list
+apt-get update && apt-get install -y spotify-client
+
+
+##nano /etc/NetworkManager/NetworkManager.conf
