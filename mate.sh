55caaf0de1a1d40b91af0f4abec297419265f68e
arielderkonig <52009875+arielderkonig@users.noreply.github.com>
2022-03-30 21:05:34 -0300

    Update mate.sh

--- mate.sh
+++ mate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "#deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main" > /etc/apt/source-list.d/pgdg.list
-echo "#deb http://nightly.odoo.com/13.0/nightly/deb/ ./" > /etc/apt/source-list.d/odoo.list
+echo "#deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main" > /etc/apt/source.list.d/pgdg.list
+echo "#deb http://nightly.odoo.com/13.0/nightly/deb/ ./" > /etc/apt/source.list.d/odoo.list
 apt-get update && apt-get upgrade
 ##===================================================================
 ## ==== INSTALLING MATE DESKTOP ENVIRONMENT - DEBIAN 10
